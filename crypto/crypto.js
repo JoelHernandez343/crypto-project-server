9874a9c6d5d65c6ad897667d136ad31e151b0e1f
Joel Hernandez <joel.programador@gmail.com>
2020-06-30 23:12:34 -0500

    Finished project :fire: :bowtie:

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -11,19 +11,20 @@ const primes = [
   6847944682037444681162770672798288913849n,
   4146162919458530168953357282201621124057n,
   5570373270183181665098052481109678989411n,
+  undefined,
 ];
 
 const e = 2946061206446183136035364744505844247510411120867004678223655427763264058485174539n;
 
-function Y(x, n) {
+function Y(x, n, d) {
   // x^d mod n
   let y = bigintModArith.modPow(x, d, n);
   return y;
 }
 
 function keyGeneration() {
-  let p = primes[Math.random() * (primes.length - 1)];
-  let q = primes[Math.random() * (primes.length - 1)];
+  let p = primes[Math.floor(Math.random() * (primes.length - 1))];
+  let q = primes[Math.floor(Math.random() * (primes.length - 1))];
   n = p * q;
   let phi = (p - 1n) * (q - 1n);
   let d = bigintModArith.modInv(e, phi);
@@ -37,3 +38,8 @@ function keyGeneration() {
 
   return { n, d };
 }
+
+module.exports = {
+  Y,
+  keyGeneration,
+};
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { Y, keyGeneration } = require('./crypto/crypto');
+
 const router = express.Router();
 const app = express();
+
+const { n, d } = keyGeneration();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get('/public', (req, res) =>
-  res.send(JSON.stringify({ publicKey: 'LLAVEJJFOISDJFOIJDFLISDIJFOID' }))
-);
+router.get('/public', (req, res) => {
+  console.log('Pedido n, enviando n...');
+
+  res.send(JSON.stringify({ publicKey: n.toString('16') }));
+});
 
 router.post('/key', (req, res) => {
-  console.log(req.body);
+  console.log('Pedido y:');
 
-  res.send(JSON.stringify({ y: 'LLAVEJJFOISDJFOIJDFLISDIJFOID' }));
+  let x = BigInt(`0x${req.body.x}`);
+  let y = Y(x, n, d).toString('16');
+
+  console.log('Enviando y...');
+
+  res.send(JSON.stringify({ y }));
 });
 
 app.use('/', router);
